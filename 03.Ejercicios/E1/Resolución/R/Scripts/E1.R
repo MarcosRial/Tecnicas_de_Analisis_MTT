e68c3eb6e1e53f1d88ff9ada96089ef6c23b6c88
MarcosRial <marcos.rial.docampo@gmail.com>
2015-10-20 11:32:31 +0200

    Aplicación de cambios sugeridos en el script
    Cambios en el redondeo de los votantes estimados

--- 03.Ejercicios/E1/Resolución/R/Scripts/E1.R
+++ 03.Ejercicios/E1/Resolución/R/Scripts/E1.R
@@ -7,21 +7,17 @@
 #n=502, error de muestreo +/-4.5%
 #Aplicación de prop.test para definir los intervalos de confianza
 #PP con 33.7% de los votos
-prop.test(169.174,502,
-          conf.level = 0.95,
-          alternative = "two.sided")
+prop.test(round(0.337*502, digits = 0),502,
+          conf.level = 0.95)
 
 #Cs con 13.8% de los votos
-prop.test(69.276,502,
-          conf.level = 0.95,
-          alternative = "two.sided")
+prop.test(round(0.138*502, digits = 0),502,
+          conf.level = 0.95)
 
 #AM con 29.1% de los votos
-prop.test(146.082,502,
-          conf.level = 0.95,
-          alternative = "two.sided")
+prop.test(round(0.291*502, digits = 0),502,
+          conf.level = 0.95)
 
 #PSOE con 17.4% de los votos
-prop.test(87.348,502,
-          conf.level = 0.95,
-          alternative = "two.sided")
+prop.test(round(0.174*502, digits = 0),502,
+          conf.level = 0.95)
