b51f7df021d431424988050520f48bb1c7699b76
MarcosRial <marcos.rial.docampo@gmail.com>
2015-11-09 20:11:32 +0100

    Evolución ejercicio 3

--- 03.Ejercicios/E3/Resolución/R/Scripts/E3.R
+++ 03.Ejercicios/E3/Resolución/R/Scripts/E3.R
@@ -8,24 +8,46 @@
 # Cargamos los datos
 datos <- read.csv("Datos/DatosExerc2.csv")
 
+# Eliminado de la variable categórica X
+datos2 <- datos[,-1]
+
 # Escalado de datos
-datos2 <- scale(datos)
-summary(datos2)
+datos3 <- scale(datos2)
+summary(datos3)
+
+#### Relación entre las 3 variables ####
+plot(datos2, main = "Relación entre variables",
+     pch = 21, bg = "grey",
+     cex = 0.8, cex.axis = 0.8)
 
 #### Método de agrupamiento jerárquico ####
 # Cálculo de distancias euclídeas
-distancias <- dist(datos2)
+distancias <- dist(datos3)
 
 # Agrupamientos y generación del dendrograma
-agrup <- hclust(distancias, method = "ward")
+agrup <- hclust(distancias, method = "ward.D")
 plot(agrup, hang = -1, cex = 0.5,
      main = "Agrupamientos",
      xlab = "", sub = "")
+abline(h = 5, col = "red", lty = 2, lwd = 1)
 
 # Generación de grupos (k=6)
-grupos1 <- cutree(agrup, k = 6)
+grupos1 <- cutree(agrup, k = 5)
 
-#### Método de agrupamiento no jerárquico ####
+# Análisis de los grupos con boxplot
+boxplot(abandon.uaa ~ grupos1, data = datos2,
+        ylab = "Abandono de superficie agrícola",
+        xlab = "Grupo")
+
+boxplot(elevation ~ grupos1, data = datos2,
+        ylab = "Elevación (msnm)",
+        xlab = "Grupo")
+
+boxplot(pop.dens ~ grupos1, data = datos2,
+        ylab = "Densidad de población (hab./km2)",
+        xlab = "Grupo")
+
+#### Método de agrupamiento no jerárquico k-means####
 
 #### Exportado de gráficos ####
 png("Graficos/Dendrograma.png", #cambiar según convenga
