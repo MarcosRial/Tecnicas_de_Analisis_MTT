34568275fa09871adb704f4798752d3482d136fd
MarcosRial <marcos.rial.docampo@gmail.com>
2015-11-12 20:15:17 +0100

    Avanzado y mejorado el script y gráficas
    Mejoradas las gráficas y creada una impresión triple de las relaciones

--- 03.Ejercicios/E3/Resolución/R/Scripts/E3.R
+++ 03.Ejercicios/E3/Resolución/R/Scripts/E3.R
@@ -11,23 +11,52 @@ datos <- read.csv("Datos/DatosExerc2.csv")
 # Eliminado de la variable categórica X
 datos2 <- datos[,-1]
 
-# Escalado de datos
-datos3 <- scale(datos2)
-summary(datos3)
-
 #### Relación entre las 3 variables ####
 plot(datos2, main = "Relación entre variables",
      pch = 21, bg = "grey",
      cex = 0.8, cex.axis = 0.8)
 
+# Otro método
+triple <- par(mfrow = c(3,1))
+par(triple)
+plot(abandon.uaa~pop.dens, data = datos2,
+     col = "darkgoldenrod3", bg = "azure4", pch = 21,
+     xlab = "Densidad de pob. (hab./km2)",
+     ylab = "Abandono de sup. agrícola")
+plot(abandon.uaa~elevation, data = datos2,
+     col = "darkgoldenrod3", bg = "sienna", pch = 21,
+     xlab = "Elevación (msnm)",
+     ylab = "Abandono de sup. agrícola")
+plot(pop.dens~elevation, data = datos2,
+     col = "darkolivegreen1", bg="burlywood3", pch = 21,
+     xlab = "Elevación (msnm)",
+     ylab = "Densidad de pob. (hab./km2)")
+
+#### Estudio de correlación entre variables ####
+# H0 = las variables no están correlacionadas
+# Abandono vs densidad de población
+cor.test(datos2$abandon.uaa, datos2$pop.dens,
+         method = "pearson", conf.level = 0.95)
+# Abandono vs elevación
+cor.test(datos2$abandon.uaa, datos2$elevation,
+         method = "pearson", conf.level = 0.95)
+# Elevacion vs densidad de población
+cor.test(datos2$elevation, datos2$pop.dens,
+         method = "pearson", conf.level = 0.95)
+
+#### Escalado de datos ####
+datos3 <- scale(datos2)
+summary(datos3)
+
 #### Método de agrupamiento jerárquico ####
 # Cálculo de distancias euclídeas
-distancias <- dist(datos3)
+distancias <- dist(datos3, method = "euclidean")
 
 # Agrupamientos y generación del dendrograma
 agrup <- hclust(distancias, method = "ward.D")
 plot(agrup, hang = -1, cex = 0.5,
      main = "Agrupamientos",
+     ylab = "Peso",
      xlab = "", sub = "")
 abline(h = 5, col = "red", lty = 2, lwd = 1)
 
